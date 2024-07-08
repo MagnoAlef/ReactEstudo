0342a015975fde55d5627363b7fa8460f0406b2a
Magno <magno.alefb@gmail.com>
2024-07-08 18:48:13 -0300

    first commit

---
+++ todo/src/components/Filter.jsx
@@ -0,0 +1,24 @@
+const Filter = ({ filter, setFilter, setSort }) => {
+  return (
+    <div className="filter">
+      <h2>Filtrar:</h2>
+      <div className="filter-options">
+        <div>
+          <p>Status:</p>
+          <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+            <option value="All">Todas</option>
+            <option value="Completas">Completas</option>
+            <option value="Incompletas">Incompletas</option>
+          </select>
+        </div>
+        <div>
+          <p>Ordem alfabetica:</p>
+          <button onClick={() => setSort("Asc")}>Asc</button>
+          <button onClick={() => setSort("Desc")}>Desc</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Filter;
